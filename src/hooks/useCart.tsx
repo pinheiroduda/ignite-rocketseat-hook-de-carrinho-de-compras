6bf76c5fe8faa5e6aac535084d2b4fc27a7ea812
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-15 18:45:18 -0300

    feat: creates functionality to add products to cart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -34,9 +34,36 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.map(
+        product => ({...product})
+      )
+      const productExists = updatedCart.find(product => product.id === productId)
+      
+      const stock = await api.get(`/stock/${productId}`)
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+      
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`/product/${productId}`);
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        }
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
